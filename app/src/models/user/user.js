2d7b7e7393ec8a38711bc3d7281b256d23b94108
Your Name <you@example.com>
2022-09-14 18:25:36 +0900

    Add(auth): email, nickname 중복 확인 체크

--- app/src/models/user/user.js
+++ app/src/models/user/user.js
@@ -9,10 +9,46 @@ class User {
         this.body = req.body;
     }
 
+    async emailConfrim(email) {
+        try {
+            const confirm = await UserStorage.emailConfirm(email);
+            if(confirm.length) {
+                return { success: false, msg: `${email}은 존재하는 email 입니다.` };
+            }
+
+            return { success: true };
+        } catch(err) {
+            throw { err };
+        }
+    }
+
+    async nicknameConfirm(nickname) {
+        try {
+            const confirm = await UserStorage.nicknameConfirm(nickname);
+            if(confirm.length) {
+                return { success: false, msg: `${nickname}은 존재하는 nickname 입니다.` };
+            };
+
+            return { success: true };
+        } catch(err) {
+            throw { err };
+        }
+    }
+
     async signUp() {
         try{
             const profile = this.body;
             
+            const emailConfrim = await this.emailConfrim(profile.email);
+            if(!emailConfrim.success) {
+                return emailConfrim;
+            }
+
+            const nicknameConfirm = await this.nicknameConfirm(profile.nickname);
+            if(!nicknameConfirm.success) {
+                return nicknameConfirm;
+            }
+
             const schoolConfirm = await school.confirm(profile.school_no);
             if(!schoolConfirm.success) {
                 return schoolConfirm;
@@ -28,10 +64,10 @@ class User {
 
             const affectedRows = await UserStorage.signUp(profile, salt);
             if (affectedRows) {
-                return {success: true, msg: '회원가입이 성공적으로 이루어 졌습니다.'};                
+                return { success: true, msg: '회원가입이 성공적으로 이루어 졌습니다.' };                
             };
 
-            return {success: false, msg: '회원가입이 실패하였습니다.'};
+            return { success: false, msg: '회원가입이 실패하였습니다.' };
         } catch(err) {
             throw { err };
         };
